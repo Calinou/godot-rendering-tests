3d9365badd811754e30a28b83226515a4b9b0a34
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-12-27 23:49:40 +0100

    Add mesh transparency, StandardMaterial3D proximity fade and distance fade tests

---
+++ tests/3d/test_standard_material_3d_distance_fade_pixel_alpha.gd
@@ -0,0 +1,16 @@
+@tool
+extends WorldEnvironment
+
+
+func _ready() -> void:
+	for x in 11:
+		for z in 11:
+			var mi = MeshInstance3D.new()
+			mi.position = Vector3(10 - x * 2, 0, 10 - z * 2)
+			mi.mesh = SphereMesh.new()
+			var material = StandardMaterial3D.new()
+			material.distance_fade_mode = StandardMaterial3D.DISTANCE_FADE_PIXEL_ALPHA
+			material.distance_fade_min_distance = 5
+			material.distance_fade_max_distance = 15
+			mi.set_surface_override_material(0, material)
+			add_child(mi)
---
+++ tests/3d/test_standard_material_3d_proximity_fade.gd
@@ -0,0 +1,15 @@
+@tool
+extends WorldEnvironment
+
+
+func _ready() -> void:
+	for x in 11:
+		for z in 11:
+			var mi = MeshInstance3D.new()
+			mi.position = Vector3(10 - x * 2, 0, 10 - z * 2)
+			mi.mesh = SphereMesh.new()
+			var material = StandardMaterial3D.new()
+			material.proximity_fade_enable = true
+			material.proximity_fade_distance = 2 * x
+			mi.set_surface_override_material(0, material)
+			add_child(mi)
