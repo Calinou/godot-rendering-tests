fb039bf0c8564bb67464e6e5795d296448bf5d3e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-02-10 03:05:53 +0100

    Fix GDScript warnings

--- tests/3d/test_clustering.gd
+++ tests/3d/test_clustering.gd
@@ -15,22 +15,25 @@ func _ready() -> void:
 	# Set a fixed random seed to ensure determinism across runs.
 	seed(0x60d07)
 
-	for __ in NUMBER_OF_ELEMENTS / 4:
+	@warning_ignore("integer_division")
+	for __ in roundi(NUMBER_OF_ELEMENTS * 0.25):
 		var omni_light := OmniLight3D.new()
-		omni_light.light_color = Color().from_hsv(randf_range(0.0, 0.3333), 1.0, 1.0)
+		omni_light.light_color = Color.from_hsv(randf_range(0.0, 0.3333), 1.0, 1.0)
 		omni_light.light_energy = 3.0
 		omni_light.position = Vector3(randf_range(-PLANE_SIZE * 0.5, PLANE_SIZE * 0.5), 2.5, randf_range(-PLANE_SIZE * 0.5, PLANE_SIZE * 0.5))
 		add_child(omni_light)
 
-	for __ in NUMBER_OF_ELEMENTS / 4:
+	@warning_ignore("integer_division")
+	for __ in roundi(NUMBER_OF_ELEMENTS * 0.25):
 		var spot_light := SpotLight3D.new()
-		spot_light.light_color = Color().from_hsv(randf_range(0.5, 0.83333), 1.0, 1.0)
+		spot_light.light_color = Color.from_hsv(randf_range(0.5, 0.83333), 1.0, 1.0)
 		spot_light.light_energy = 3.0
 		spot_light.position = Vector3(randf_range(-PLANE_SIZE * 0.5, PLANE_SIZE * 0.5), 2.5, randf_range(-PLANE_SIZE * 0.5, PLANE_SIZE * 0.5))
 		spot_light.rotation = Vector3(-1, 0, 0)
 		add_child(spot_light)
 
-	for __ in NUMBER_OF_ELEMENTS / 4:
+	@warning_ignore("integer_division")
+	for __ in roundi(NUMBER_OF_ELEMENTS * 0.25):
 		var decal := Decal.new()
 		decal.texture_albedo = preload("res://icon.png")
 		decal.extents *= 2.0
@@ -39,10 +42,11 @@ func _ready() -> void:
 		decal.rotation = Vector3(randf_range(0, 3), randf(), randf())
 		add_child(decal)
 
-	for __ in NUMBER_OF_ELEMENTS / 4:
+	@warning_ignore("integer_division")
+	for __ in roundi(NUMBER_OF_ELEMENTS * 0.25):
 		var reflection_probe := ReflectionProbe.new()
 		reflection_probe.extents *= 0.5
 		reflection_probe.ambient_mode = ReflectionProbe.AMBIENT_COLOR
-		reflection_probe.ambient_color = Color().from_hsv(0.0, 0.0, randf_range(-2, 3))
+		reflection_probe.ambient_color = Color.from_hsv(0.0, 0.0, randf_range(-2, 3))
 		reflection_probe.position = Vector3(randf_range(-PLANE_SIZE * 0.5, PLANE_SIZE * 0.5), 0.0, randf_range(-PLANE_SIZE * 0.5, PLANE_SIZE * 0.5))
 		add_child(reflection_probe)
